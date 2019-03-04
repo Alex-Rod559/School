8961fe7938a557594a8714382d99d58f5a676ad1
Alex-Rod559 <48215081+Alex-Rod559@users.noreply.github.com>
2019-03-04 10:17:14 -0500

    test commit

---
+++ homeworkch3.py
@@ -0,0 +1,22 @@
+'''
+spamm = input("type food: ")
+
+if spamm == 'spamm':
+    print('Ummmm, my favorite!')
+    print('I feel like saying it 100 times...')
+    print(100 * (spamm + '! '))
+'''
+
+
+print ("The following items are on sale 20 percent off: cake, muffins, and bread")
+FoodCollection = input("Plaese enter 5 food iteams seperated by comma: ")
+Food_List = FoodCollection.split(", ")
+
+
+
+
+#print("Printing Food")
+#for food in Food_List:
+#    print(food)
+
+#QuantCollection = int(input("Plesae type how many of "))
---
+++ test.py
@@ -0,0 +1,13 @@
+"""
+print ("Folurecent Adolece")
+
+coderunner = input("Type out whatever: ")
+print (coderunner)
+"""
+input_string = input("Enter family members separated by comma ")
+family_list  = input_string.split(",")
+print("Printing all family member names")
+for name in family_list:
+     print(name)
+
+
