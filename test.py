0f0bf0bbbf2f5ad9f578263004a9140590e0a475
alex-rod559 <48215081+Alex-Rod559@users.noreply.github.com>
2019-03-05 09:02:04 -0500

    done

--- test.py
+++ test.py
@@ -2,24 +2,17 @@
 #3/5/2019
 #Program: Allows users to pick the amount of something and it outputs how much they got and how much it cost 
 
-items = ('cake', 'pie', 'cookies', 'bread', 'muffins')
-
-
 cake1 = int(input("Type amount of cakes you want: "))
 pie1 = int(input("Type amount of pies you want: "))
 cookies1 = int(input("Type amount of cookies you want: "))
 bread1 = int(input("Type amount of loafs of bread you want: "))
 muffins1 = int(input("Type amount of muffins you want: "))
 
-quant = cake1, pie1, cookies1, bread1, muffins1
-
 cake_price = cake1 * 3 
 pie_price = pie1 * 6
 cookie_price = cookies1 * 2
 bread_price = bread1 * 4
 muffins_price = muffins1 * 5
-
-item_price = (cake_price, pie_price, cookie_price, bread_price, muffins_price)
 
 discountCake = cake_price * .2
 discountPie = pie_price * .3
@@ -28,15 +21,21 @@ discountBread = bread_price * .4
 discountMuffin = muffins_price * .1
 
 dicountList = ('20%','30%','50%','40%','10%')
-
+item_price = (cake_price, pie_price, cookie_price, bread_price, muffins_price)
+quant = cake1, pie1, cookies1, bread1, muffins1
+items = ('cake', 'pie', 'cookies', 'bread', 'muffins')
 discountTotal = discountBread + discountCake + discountCookie + discountMuffin + discountPie 
+
+
 
 print ("%4s %16s %16s %20s" % \
     ("Item", "Quantity","Selling Price", "Discount in Percent"))
 
 
+
 for item, quantity, price, discount in zip(items, quant , item_price, dicountList):
     print("%-7s%12s%13s%14s" % \
         (item, quantity, price, discount))
+
 
 print ("Total: $ %0.2f" % discountTotal, "USD" )
